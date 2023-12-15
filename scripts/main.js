65ab917f6318e1bfb10ff520922a37b69e458f62
majel347 <111613095+majel347@users.noreply.github.com>
2023-12-15 15:12:17 -0500

    due

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,32 @@
- //Images
+//--------------------------------------------------------------
+
+//OPEN NOISE DIV FUNCTION
+let openBtn = document.getElementById("openBtn");
+let noiseContainer = document.getElementById("noise");
+let noiseBox = document.getElementById("video");
+let vidBtn = document.getElementById("vidBtn");
+
+noiseBox.src
+//CLOSING + OPENING  NOISE BOX
+const closeBtn = document.getElementById("closeBtn");
+const closeIt = () => { 
+    noiseContainer.style.display = "none";
+    closeBtn.style.display = "none";
+} 
+ closeBtn.addEventListener("click", closeIt);
+
+
+ let openIt = () => {  
+    noiseContainer.style.display = 'block'; 
+    closeBtn.style.display = 'inline';
+    noiseBox.play(); 
+};
+  
+ //--------------------------------------------------------------
+
+
+
+//Images
  
     let imgs = [];
     imgs.push ({
@@ -28,19 +56,31 @@
         video: null,
     });
    
+ imgs.push ({
+    origin: 'czech republic',
+     name: 'math',
+        link: "assets/cz/cz_math.png",
+        video: null,
+    });
+   
     imgs.push ({
         origin: 'czech republic',
-        name: 'deer',
-        link: "assets/cz/cz_deer_x.png",
-        video: "assets/cz/cz_deer_video.mov",
-    });
+         name: 'bear',
+            link: "assets/cz/cz_bear.png",
+            video: null,
+        });
     imgs.push ({
         origin: 'czech republic',
-        name: 'boy with hoop',
-        link: "assets/cz/cz_hoopboy_x.png",
-        video: "assets/cz/cz_hoopboy_video.mov",
-    });
-
+         name: 'preteen',
+            link: "assets/cz/cz_preteen.png",
+            video: null,
+        });
+        imgs.push ({
+            origin: 'czech republic',
+             name: 'teen',
+                link: "assets/cz/cz_teen.png",
+                video: null,
+            });
     imgs.push ({
         origin: 'czech republic',
         name: 'farm break',
@@ -68,8 +108,6 @@
         link: "assets/cz/cz_handsome.png",
         video: null,
     });
-
-    
 
     imgs.push ({
         origin: 'czech republic',
@@ -99,12 +137,12 @@
         video: null,
     });
 
-    imgs.push ({
-        origin: 'czech republic',
-        name: 'on the pier',
-        link: "assets/cz/cz_pier_x.png",
-        video: "assets/cz/cz_pier_video.mov"
-    });
+ imgs.push ({
+  origin: 'czech republic',
+  name: 'horses',
+  link: "assets/cz/cz_horses.png",
+   video: null,
+ });
 
     imgs.push ({
         origin: 'czech republic',
@@ -135,37 +173,38 @@
         video: null,
     });
 
-    imgs.push ({
-        origin: 'czech republic',
-        name: 'walking friends',
-        link: "assets/cz/cz_walking_x.png",
-        video:"assets/cz/cz_walking_video.mov"
-    });
+ imgs.push ({
+ origin: 'czech republic',
+ name: 'gun',
+ link: "assets/cz/cz_gun.png",
+  video:null,
+  });
 
-    imgs.push ({
-        origin: 'italy',
-        name: 'arch view',
-        link: "assets/italy/italy_venice1_x.png",
-        video:"assets/italy/italy_venice_vid1.mp4",
-    });
+ imgs.push ({
+   origin: 'czech republic',
+     name: 'class',
+       link: "assets/cz/cz_class.png",
+    video:null,
+  });
 
-    let noiseImgs = [];
+ 
   
-
 //find the html location
 let container = document.getElementById('arrayCtr');
+
+//--------------------------------------------------------------
 
 //for each loop will go through each item in the array
 
 imgs.forEach(function(d){
   let newImg = document.createElement('img');
-   newImg.src = d.link; 
-  newImg.style.left = Math.random() * 40  + '%';
-    newImg.style.top = Math.random() * 40  + '%';
+  newImg.src = d.link; 
+  newImg.style.left = Math.random() * 45  + '%';
+newImg.style.top = Math.random() * 50  + '%';
 
 //ALL IMAGES
 //for each click  add zoom effect on click
- 
+
     newImg.addEventListener('click',function() {
      
     if(newImg.classList.contains('imgZoom')) 
@@ -184,70 +223,73 @@ newImg.addEventListener('click',function() {
         newImg.style.zIndex = 120;  
     } 
     else { 
-        newImg.style.zIndex = 3; 
+        newImg.style.zIndex = 1; 
     }      
 });
     container.appendChild(newImg);    
-
-
-//ANIMATION IMAGES ONLY
-//for each image loop to roll over with border for video images
+});
 
  
-newImg.addEventListener('mouseover',function() {
-    if(d.video.assets) 
-        { 
-            newImg.classList.add("hoverstate"); 
-        } 
-        else { 
-            newImg.classList.add("hoverstate"); 
-        }  
-    });
+
+ //VIDEO STUFF
+
+   /**click function for the image of the image **/
+   /** WET CODE ALERT */
+ 
+
+var videosList = [
+    "assets/cz/cz_deer_video.mov",
+    "assets/cz/cz_hoopboy_video.mov",
+    "assets/cz/cz_pier_video.mov",
+    "assets/cz/cz_walking_video.mov",
+];
+
+window.onload = function() {
+    // for videos
+      var vid = document.createElement('source');
+      vid.src = videosList[0]; // playing first video in the array by default
+      document.getElementById('video').appendChild(vid)};  
+
+clickFn = function(e){
+        var video = document.getElementById('video');
+        video.src = videosList[parseInt(e.srcElement.id,4)];
+        
+        noiseContainer.style.display = 'block'; 
+        closeBtn.style.display = 'inline';
+        video.getElementById.video.play();
+   } ;
+ 
+var videoImgs = [
+    "assets/cz/cz_deer_x.png", 
+    "assets/cz/cz_hoopboy_x.png", 
+    "assets/cz/cz_pier_x.png", 
+    "assets/cz/cz_walking_x.png" ];
+
+ 
+var allPics=videoImgs.length;
+
+     
+var i = 0;
+
+for (; i < allPics; i++) {
+  var a = document.createElement('a');
+  // a.href = 'example.html';
+  var img = document.createElement('img');
+  img.src = videoImgs[i];
+  img.id = i; // for the reference of clicked image
+  a.appendChild(img);
+  document.getElementById('arrayCtr').appendChild(a);
+  img.style.left = Math.random() * 45  + '%';
+  img.style.top = Math.random() * 45  + '%' ; 
+  
+ 
+  a.addEventListener("click", clickFn, false); 
+    };     
+ 
     
-    newImg.addEventListener('mouseout',function() {
-        if(d.video.assets) 
-            { 
-                newImg.classList.contains("hoverstate"); 
-            } 
-            else { 
-                newImg.classList.remove("hoverstate"); 
-            }  
-        });
-//OPEN NOISE DIV FUNCTION
-let openBtn = document.getElementById("openBtn");
-let noiseContainer = document.getElementById("noise");
- 
 
-//CLOSING + OPENING  NOISE BOX
-const closeBtn = document.getElementById("closeBtn");
-const closeIt = () => { 
-    noiseContainer.style.display = "none";
-    closeBtn.style.display = "none";
-} 
- closeBtn.addEventListener("click", closeIt);
-
-
- let openIt = () => {  
-    noiseContainer.style.display = 'block'; 
-     closeBtn.style.display = 'inline';};
-      
-//test button:
-openBtn.addEventListener("click", openIt);
  
- 
-//images as buttons
- 
-newImg.addEventListener("click",function() {
-    if(d.video.assets) { 
-      noiseContainer.style.display = 'block';
-      noiseContainer.style.zIndex = "100";
-     closeBtn.style.display = 'inline';
-}  
-    });
- 
-});        
- 
- 
+ //--------------------------------------------------------------
 
  //REMIX FUNCTION
 
@@ -258,15 +300,3 @@ const newArray = ()=> {
 restartBtn.addEventListener ("click", newArray);
  
 console.log ("hello world??");
- 
- 
-//video play 
-
-clickFn = function(e){
-    var video = document.getElementById('video');
-    video.src = videosList[parseInt(e.srcElement.id,10)];
-    video.play();
-}
-
-
- 
